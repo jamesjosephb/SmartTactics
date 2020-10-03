416e1d46da5917f7316c8b1fa6e48e74a6ee17da
James Burch <burchjames90@gmail.com>
2020-10-03 17:27:19 -0600

    First Commit, Building Level

---
+++ Assets/Scripts/FieldManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FieldManager : MonoBehaviour
+{
+    Vector3 testPos;
+
+
+    Transform testTrans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        testPos = transform.position;
+        print(testPos);
+        testPos = new Vector3(0f, 10f, 0f);
+        print(testPos);
+        testTrans = transform;
+        testTrans.position = new Vector3(0f, 10f, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
